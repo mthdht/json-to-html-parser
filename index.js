6b58ec8a4d33c6f7d4d64f11c823cea8acc3a17a
mthdht <mthdht@gmail.com>
2019-07-09 14:37:04 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
 class JsonConverter {
     constructor() {
-        this.selfClosingTags = ["area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"],
-        this.json = {
-            tag: "div",
-            attributes: {
-                class: "container"
-            },
-            children: [
-                {
-                    tag: "p",
-                    attributes: {
-                        id: "first"
-                    },
-                    children: [
-                        {
-                            tag: "span",
-                            innerHtml: "bold"
-                        },
-                        {
-                            innerHtml: "welcome"
-                        },
-                        {
-                            tag:"span",
-                            innerHtml: "italic"
-                        }
-                    ]
-                },
-                {
-                    tag: "p",
-                    innerHtml: "second"
-                }
-            ]
-        }
+        this.selfClosingTags = ["area", "base", "br", "col", "command", "embed", "hr", "img", "input", "keygen", "link", "meta", "param", "source", "track", "wbr"]
     }
     convertToElement(element) {
+        // check if the element has 'tag' attribute
         if (Object.keys(element).indexOf('tag') == -1) {
-            return element.innerHtml
+            return element.content
         }
 
-        if (Object.keys(element).indexOf('innerHtml') !== -1) {
-            return this.getStart(element) + element.innerHtml + this.getEnd(element)
+        if (Object.keys(element).indexOf('content') !== -1) {
+            return this.getStart(element) + element.content + this.getEnd(element)
         }
 
         if (Object.keys(element).indexOf('children') !== -1) {
@@ -49,7 +19,7 @@ class JsonConverter {
             });
             return this.getStart(element) + string + this.getEnd(element)
         }
-        return this.getStart(element) + this.selfClosingTags.indexOf(element.tag) == -1 ? this.getEnd(element) : '' 
+        return this.getStart(element) + (this.selfClosingTags.indexOf(element.tag) == -1 ? this.getEnd(element) : '') 
     }
     attributesParser(attributes) {
         let attributesString = ""
@@ -72,6 +42,4 @@ class JsonConverter {
 
 }
 
-let test = new JsonConverter()
-console.log(test.convertToElement(test.json))
-//export default new JsonConverter()
+export default new JsonConverter()
