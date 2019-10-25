584074cf563805c81069da373337b270f9796d23
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2019-10-25 20:45:45 +0200

    test(attributesParser): add test for attributesParser function

--- test/jsConverter.test.js
+++ test/jsConverter.test.js
@@ -19,3 +19,19 @@ describe('convertToElement', () => {
     expect(returnValue).toBe('<p class="classe1 classe2" title="Some title">My p content</p>');
   });
 });
+
+describe('attributesParser', () => {
+  test('should return attributes to html element', () => {
+    // GIVEN
+    const enterValue = {
+      class: 'classe1 classe2',
+      title: 'Some title'
+    };
+
+    // WHEN
+    const returnValue = JsonConverter.attributesParser(enterValue);
+
+    // THEN
+    expect(returnValue).toBe(' class="classe1 classe2" title="Some title"');
+  });
+});
